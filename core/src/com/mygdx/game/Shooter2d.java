4b0ecdbed4e3bc470e13f37d6b3af663c17133d2
Adson098 <asmierciak098@gmail.com>
2017-11-19 18:24:59 +0100

    12.11.2017 18.24 backup

--- core/src/com/mygdx/game/Shooter2d.java
+++ core/src/com/mygdx/game/Shooter2d.java
@@ -5,13 +5,20 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.mygdx.game.State.LoadingState;
 
 
 public class Shooter2d extends Game {
+	public static final int V_WIDTH = 1280;
+	public static final int V_HEIGHT = 720;
+	private final AssetManager assetManager = new AssetManager();
 	@Override
 	public void create() {
-		setScreen(new GameScreen(this));
+		assetManager.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
+		setScreen(new LoadingState(this));
 
 	}
-
+	public AssetManager getAssetManager() {
+		return assetManager;
+	}
 }
--- core/src/com/mygdx/game/State/GameScreen.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.game.State;
-
-/**
- * Created by Adson Work on 17.11.2017.
- */
-public class GameScreen {
-
-
-}
