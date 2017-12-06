dbc136c996bea91061804b80548f52129cd0b9d9
Adson098 <asmierciak098@gmail.com>
2017-12-06 20:52:54 +0100

    06.12.2017 20.52 Backup

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,5 +8,7 @@ public class  DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Shooter2d(), config);
+		config.width = Shooter2d.V_WIDTH;
+		config.height = Shooter2d.V_HEIGHT;
 	}
 }
